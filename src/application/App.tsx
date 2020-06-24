85d41cde259f74b90bc61b9eeb957f5ac23d7178
Hardelele <Hardelele@yahoo.com>
2020-06-24 22:48:45 +0300

    Change architecture

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Product from "./components/Product";
 
 function App() {
   return (
     <div className="App">
-
+      <Product/>
     </div>
   );
 }
