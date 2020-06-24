ece652539d7bd8654142899a7110a33407ec4107
Hardelele <Hardelele@yahoo.com>
2020-06-25 02:42:07 +0300

    Add Home page

---
+++ src/application/pages/Home.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Product from "../components/product/Product";
+
+const Home = () => {
+    return (
+        <div className="home-page">
+            <Product/>
+        </div>
+    );
+};
+
+export default Home;
