2e4a91ba22cf9c365dccbfdf6d1a6e35f8db6a96
Hardelele <Hardelele@yahoo.com>
2020-06-25 02:41:06 +0300

    Add logo component

---
+++ src/application/components/header/logo/Header.logo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './header.logo.css';
+
+function HeaderLogo() {
+    return (
+        <div className="header-logo">Russian Honey</div>
+    );
+}
+
+export default HeaderLogo;
