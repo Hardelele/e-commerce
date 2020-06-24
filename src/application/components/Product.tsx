cce34c2457510690bb1355e06fa0505b7f06bc7c
XXXLOLIHUNTERXXX <netherlance@bk.ru>
2020-06-24 23:37:55 +0300

    Remove extra files

--- src/application/components/Product.tsx
+++ src/application/components/Product.tsx
@@ -4,6 +4,10 @@ import './product.css'
 function Product() {
     return (
         <div className="product">
+            <div className="product-name">Mid</div>
+            <div className="image-product"/>
+            <div className="product-prise">1000$</div>
+
         </div>
     );
 }
