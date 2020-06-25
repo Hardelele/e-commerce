a225627b071e7cb984e93a9d74380f9c3111fe90
Hardelele <Hardelele@yahoo.com>
2020-06-25 10:08:00 +0300

    Introduced new components

--- src/application/components/header/Header.tsx
+++ src/application/components/header/Header.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import './header.css';
 import HeaderLogo from "./logo/Header.logo";
-import ExpandButton from "../user/buttons/expand/Expand.button";
+import Search from "./search/Search";
+import CartButton from "./cart/Cart.button";
+import UserMenu from "./user/menu/UserMenu";
 
 function Header() {
     return (
         <div className="header">
             <HeaderLogo/>
-            <ExpandButton/>
+            <Search/>
+            <CartButton/>
+            <UserMenu/>
         </div>
     );
 }
